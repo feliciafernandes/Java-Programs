b9ebbf1c9165098b0a7cb2d7da79cd6e5dc22a3c
Felicia Fernandes <feliciafernandes3456@gmail.com>
2025-10-07 21:46:38 +0530

    Add files via upload

---
+++ Producer-Consumer/Consumer.java
@@ -0,0 +1,15 @@
+package producerConsumer;
+
+public class Consumer extends Thread{
+	private MessageBoard board;
+	
+	public Consumer(MessageBoard b) {
+		this.board=b;
+	}
+	@Override
+	public void run() {
+		for (int i = 0; i < 3; i++) {
+			board.get();
+		}
+	}
+}
---
+++ Producer-Consumer/ProducerConsumerDemo.java
@@ -0,0 +1,17 @@
+package producerConsumer;
+
+public class ProducerConsumerDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		MessageBoard board = new MessageBoard();
+		Producer p=new Producer(board);
+		Consumer c = new Consumer(board);
+		
+		p.start();
+		c.start();
+		p.join();
+		c.join();
+		
+	}
+
+}
